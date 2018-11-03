de6de7b4cd6aff084aad641aa4da740bf0e0aebd
FrossardHeitor <heitorfrossard@gmail.com>
2018-11-03 12:48:32 -0700

    server set up

--- server/server.js
+++ server/server.js
@@ -2,3 +2,22 @@ var express = require('express');
 var app = express();
 var api = require('./api/api');
 var config = require('./config/config');
+var logger = require('./util/logger');
+require('mongoose').connect(config.db.url);
+require('./middleware/appMiddlware')(app);
+
+app.use('/api', api);
+
+app.use(function(err, req, res, next) {
+  // if error thrown from jwt validation check
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).send('Invalid token');
+    return;
+  }
+
+  logger.error(err.stack);
+  res.status(500).send('Oops');
+});
+
+// export the app for testing
+module.exports = app;
